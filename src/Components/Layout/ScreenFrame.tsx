f57e5342df03f65114f9bde080a39f9daabd1df6
jasonLapina <lapina.jason@gmail.com>
2024-12-10 10:17:24 +0800

    add screen frame borders

--- src/Components/Layout/ScreenFrame.tsx
+++ src/Components/Layout/ScreenFrame.tsx
@@ -1,4 +1,5 @@
 import AddIcon from "@mui/icons-material/Add";
+import { Box } from "@mui/material";
 
 export default function ScreenFrame() {
   return (
@@ -7,11 +8,49 @@ export default function ScreenFrame() {
       <Cross position='top-right' />
       <Cross position='bottom-left' />
       <Cross position='bottom-right' />
+
+      <NonNavLine side='bottom' />
+      <NonNavLine side='left' />
+      <NonNavLine side='right' />
     </>
   );
 }
 
-const NonNavLine = () => {};
+const NonNavLine = ({ side }: NonNavLineProps) => {
+  const styles: Record<string, unknown> = {};
+  const getStyles = (side: string) => {
+    if (side !== "bottom") {
+      styles.width = "1px";
+      styles.top = "50%";
+      styles.transform = "translateY(-50%)";
+      styles.height = "90vh";
+      if (side === "left") {
+        styles.left = "20px";
+      } else if (side === "right") {
+        styles.right = "20px";
+      }
+    } else {
+      styles.height = "1px";
+      styles.width = "95vw";
+      styles.bottom = "20px";
+      styles.left = "50%";
+      styles.transform = "translateX(-50%)";
+    }
+
+    return styles;
+  };
+
+  return (
+    <Box
+      sx={{
+        position: "fixed",
+        bgcolor: "white",
+        opacity: 0.5,
+        ...getStyles(side),
+      }}
+    />
+  );
+};
 
 const Cross = ({ position }: CrossProps) => {
   const getStyles = (position: string) => {
